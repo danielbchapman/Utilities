3c2427246cd5e357c680324b98154181475f4101
Daniel B. Chapman <chapman@danielbchapman.com>
2014-02-16 10:11:43 -0600

    - Resolved all conflicts and merged the project

--- src/main/java/com/lightassistant/utility/UtilityText.java
+++ src/main/java/com/lightassistant/utility/UtilityText.java
@@ -1,6 +1,14 @@
 package com.lightassistant.utility;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.util.Arrays;
 
 public class UtilityText
@@ -142,5 +150,25 @@ public class UtilityText
     char[] ret = text.toCharArray();
     ret[0] = Character.toLowerCase(ret[0]);
     return new String(ret);
+  }
+  
+  /**
+   * <JavaDoc>
+   * @param file
+   * @param data
+   * @throws IOException <Return Description>  
+   * 
+   */
+  //FIXME Java Doc Needed
+  public static void writeFile(File file, String data) throws IOException
+  {
+    try(BufferedWriter out = new BufferedWriter(new FileWriter(file))){
+      out.write(data);  
+    }
+  }
+  
+  public static String format(String input, Object ... args)
+  {
+    return MessageFormat.format(input, args);
   }
 }
